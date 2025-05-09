4603ed00b2084e6a8140b8cabb35abf084d88ff9
Mario Ernesto Gámez Godoy <mario.gamez58@uabc.edu.mx>
2025-05-09 18:01:34 +0000

    njaskdmsakmdas

--- getbit.asm
+++ getbit.asm
@@ -1,12 +1,14 @@
 section .text
-   getbit:
-        push ebp
-        mov ebp, esp
-        mov eax, [ebp+12]
-        mov edx, [ebp+8]
-        mov ecx, eax
-        sar edx, cl
-        mov eax, edx 
-        and eax, 1
-        pop ebp
-        ret
+global getbit
+
+getbit:
+    push ebp
+    mov ebp, esp
+    mov eax, [ebp+12]
+    mov edx, [ebp+8]
+    mov ecx, eax
+    sar edx, cl
+    mov eax, edx 
+    and eax, 1
+    pop ebp
+    ret
