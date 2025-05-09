83e2618078b5e538f162b75bddae952c700f485e
Mario Ernesto Gámez Godoy <mario.gamez58@uabc.edu.mx>
2025-05-09 18:41:24 +0000

    sasdsadsddcmaksm

--- P9/P9.asm
+++ P9/P9.asm
@@ -36,10 +36,31 @@ section .text
     strchr_dummy:
      push epb
      mov ebp,esp
-     jump .sec1
+     jump .dump1
 
-     .sec23
+     .dump5:
+      mov eax,[ebp+8]
+      movzx eax, BYTE [eax]
+      mov edx,[ebp+12]
+      cmp al,dl
+      jne .dump3
+      mov eax,[epb+8]
+      jmp .dump4
 
+    .dump3:
+      add DWORD [ebp+8],1
+
+    .dump2:
+      mov eax,[ebp+8]
+      movzx eax,BYTE [eax]
+      test al,al
+      jne .dump5
+      mov eax,0
+
+    .dump4
+     pop epb
+     ret
+        
      
     getbit:
       push ebp
