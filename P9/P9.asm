00797b6cf621f1d7ad3373c125f4a4827685ef0e
Mario Ernesto Gámez Godoy <mario.gamez58@uabc.edu.mx>
2025-05-09 18:16:43 +0000

    ksdlad

---
+++ P9/P9.asm
@@ -0,0 +1,17 @@
+section .text
+   global suma
+   global strlen_dummy
+   global strchr_dummy
+   global getbit
+
+
+ suma:
+   push ebp,esp
+   mov edx,[ebp+8]
+   mov eax,[ebp+12]
+   add eax,edx
+   pop ebp
+   ret
+
+  strlen_dummy:
+     push ebp,esp
