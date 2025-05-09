a52e80b46d6072ebeb05f48fe6d3b0cac1104f8c
Mario Ernesto Gámez Godoy <mario.gamez58@uabc.edu.mx>
2025-05-09 18:33:38 +0000

    sadmaskdas

--- P9/P9.asm
+++ P9/P9.asm
@@ -15,5 +15,40 @@ section .text
 
   strlen_dummy:
      push ebp
-     mov eb, esp
+     mov ebp, esp
+     sub esp,16
+     mov DWORD [epb-4],1
+     jmp .next
+
+    .upper:
+      add DWORD [ebp-4],1
+      add DWORD [ebp+8],1
+      
+    .next:
+      mov eax,[ebp+8]
+      movzx eax, BYTE [eax]
+      test al,al
+      jne .upper
+      mov eax,[ebp-4]
+      leave
+      ret
+
+    strchr_dummy:
+     push epb
+     mov ebp,esp
+     jump .sec1
+
+     .sec23
+
      
+    getbit:
+      push ebp
+      mov ebp,esp
+      mov eax,[ebp+12]
+      mov edx,[ebp+8]
+      mov ecx,eax
+      sar edx,cl
+      mov eax,edx
+      and eax,1
+      pop ebp
+      ret
