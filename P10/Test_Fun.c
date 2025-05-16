bbf3bd15702639420ff6d63028b7f291dc747965
Mario Ernesto Gámez Godoy <mario.gamez58@uabc.edu.mx>
2025-05-16 17:51:25 +0000

    sndaksdas

---
+++ P10/Test_Fun.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdint.h>
+
+extern void pBin8b(uint8_t dato);
+extern void pBin16b(uint16_t dato);
+extern void pBin32b(uint32_t dato);
+extern void pBin64b(uint64_t dato);
+
+int main(void){
+    printf("prueba pBin8b con valor 42")
+    pBin8b(42);
+}
