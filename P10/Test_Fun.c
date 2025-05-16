c53fa24cb4b3b0c2450995111334b2a5b4493f70
Mario Ernesto Gámez Godoy <mario.gamez58@uabc.edu.mx>
2025-05-16 18:01:55 +0000

    mnskdmsakmdksamd

--- P10/Test_Fun.c
+++ P10/Test_Fun.c
@@ -10,7 +10,7 @@ int main(void){
     printf("prueba pBin8b con valor 42 : \n");
     pBin8b(42);
     
-    
-    printf("\n prueba para pBin16b con valor 3333 : \n");
+
+    printf("\nprueba para pBin16b con valor 3333 : \n");
     pBin16b(3333);
 }
